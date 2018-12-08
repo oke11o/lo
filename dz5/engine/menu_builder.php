2060d4c15b6d95457f331f5f708f6b2bf705f99d
GreenSky87 <doc03_87@mail.ru>
2018-12-08 18:15:43 +0600

    домашнее задание 5. сделал CREATE TABLE `gallery` (   `id_gallery` int(11) NOT NULL,   `name_gallery` varchar(250) NOT NULL,   `tip_gallery` varchar(50) NOT NULL,   `way_gallery` varchar(250) NOT NULL,   `size_gallery` int(8) NOT NULL,   `counter_gallery` varchar(11) NOT NULL ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
    для того, чтобы сразу счетчик был внутри по post или getи добавил тип чтобы разделять картинки. Мне для проекта верхний и нижний ряд

---
+++ dz5/engine/menu_builder.php
@@ -0,0 +1,99 @@
+<?php
+return [
+                    [
+                        'name' => 'Home',
+                        'href' => '/',                                              
+                    ],
+                    
+                    [
+                        'name' => 'Man',
+                        'href' => '/Man.php',
+                        'podmenu' => [
+                            [
+                                'name' => 'Tees/Tank tops',
+                                'href' => '/Tees_Tank tops.php',                                
+                            ],
+                            [
+                                'name' => 'Shirts/Polos',
+                                'href' => '/Shirts_Polos.php',                              
+                            ],
+                            [
+                                'name' => 'Sweaters',
+                                'href' => '/Sweaters.php',                              
+                            ],
+                            [
+                                'name' => 'Sweatshirts/Hoodies',
+                                'href' => '/Sweatshirts_Hoodies.php',                               
+                            ],
+                            [
+                                'name' => 'Blazers',
+                                'href' => '/Blazers.php',                               
+                            ],
+                            [
+                                'name' => 'Jackets/vests',
+                                'href' => '/Jackets_vests.php',                             
+                            ],
+                           
+                        ],
+                    ],
+                    [
+                        'name' => 'Woman',
+                        'href' => '/Woman.php',
+                        'podmenu' => [
+                            [
+                                'name' => 'Dresses',
+                                'href' => '/Dresses.php',                               
+                            ],
+                            [
+                                'name' => 'Tops',
+                                'href' => '/Tops.php',                              
+                            ],
+                            [
+                                'name' => 'Sweaters/Knits',
+                                'href' => '/Sweaters_Knits.php',                                
+                            ],
+                            [
+                                'name' => 'Jackets/Coats',
+                                'href' => '/Jackets_Coats.php',                             
+                            ],
+                            [
+                                'name' => 'Blazers',
+                                'href' => '/Blazers.php',                               
+                            ],
+                            [
+                                'name' => 'Denim',
+                                'href' => '/Denim.php',                             
+                            ],
+                            [
+                                'name' => 'Leggings/Pants',
+                                'href' => '/Leggings_Pants.php',                                
+                            ],
+                            [
+                                'name' => 'Skirts/Shorts',
+                                'href' => '/Skirts_Shorts.php',                             
+                            ],
+                            [
+                                'name' => 'Accessories',
+                                'href' => '/Accessories.php',                               
+                            ],
+                        ],
+                    ],
+                    [
+                        'name' => 'Kids',
+                        'href' => '/Kids.php',  
+                    ],
+                     [
+                        'name' => 'accoseriese',
+                        'href' => '/accoseriese.php',
+                    ],
+                     [
+                        'name' => 'featured',
+                        'href' => '/featured.php',
+                    ],
+                     [
+                        'name' => 'hot deals',
+                        'href' => '/hot_deals.php', 
+                    ],
+                 
+        ];
+?>
